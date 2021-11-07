9b12f1508f1568ff473698eeaefd3d9370eb4e17
stavtime <stavtime@gmail.com>
2021-11-07 14:50:46 +0300

    Изменил название ветки

--- index.php
+++ index.php
@@ -221,6 +221,7 @@ $articles = [
                         <a class="filters__button filters__button--quote button" href="#">
                             <span class="visually-hidden">Цитата</span>
                             <svg class="filters__icon" width="21" height="20">
+
                                 <use xlink:href="#icon-filter-quote"></use>
                             </svg>
                         </a>
