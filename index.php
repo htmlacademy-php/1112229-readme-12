bb3ff26cd222b8778b8fc96534792814461a6368
stavtime <stavtime@gmail.com>
2021-11-08 22:48:52 +0300

    Внес правки

--- index.php
+++ index.php
@@ -264,7 +264,7 @@ $articles = [
                             <a class="post-link__external" href="http://<?php echo $article['content']; ?>" title="Перейти по ссылке">
                                 <div class="post-link__info-wrapper">
                                     <div class="post-link__icon-wrapper">
-                                        <img src="https://www.google.com/s2/favicons?domain=vitadental.ru" alt="Иконка">
+                                        <img src="https://www.google.com/s2/favicons?domain=vitadental.ru" alt="Иконка ">
                                     </div>
                                     <div class="post-link__info">
                                         <h3><?php echo $article['title']; ?></h3>
